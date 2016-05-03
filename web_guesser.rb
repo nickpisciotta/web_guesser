56a1def3e757282b17a0a7d942637a8eff08a460
Nick Pisciotta <npisciotta@gmail.com>
2016-05-03 12:14:21 -0600

    Web Guesser complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,25 +1,40 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-NUMBER = rand(101)
+@@number = rand(101)
+@@guesses = 5
 
 get '/' do
   guess = params["guess"].to_i
+  cheat = params["cheat"]
   response = check_guess(guess)
-  erb :index, :locals => {:number => NUMBER, :reponse => response}
+  if cheat == "true"
+    response = "The secret code is #{@@number}"
+  end
+  erb :index, :locals => {:number => @@number, :reponse => response}
 end
 
 def check_guess(guess)
-  case
-  when guess > (NUMBER + 5)
-    "Way too high"
-  when guess < (NUMBER - 5)
-    "Way too low"
-  when guess > NUMBER
-    "Too High!"
-  when guess < NUMBER
-    "Too Low!"
-  when guess = NUMBER
-    "You are correct! The secret number is #{NUMBER}"
+  @@guesses -= 1
+  if @@guesses == 0
+     @@number = rand(101)
+     @guesses = 5
+     "You've lost, choose a new number"
+  else
+    case
+    when guess > (@@number + 5)
+      "Way too high"
+    when guess < (@@number - 5)
+      "Way too low"
+    when guess > @@number
+      "Too High!"
+    when guess < @@number
+      "Too Low!"
+    when guess = @@number
+      message = "You are correct! The secret number is #{@@number}"
+      @@number = rand(101)
+      @@guesses = 5
+      message
+    end
   end
 end
